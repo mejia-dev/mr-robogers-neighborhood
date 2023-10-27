35ae5bfb723df1e2669b1cbd3e005b95c70470bd
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-27 09:50:36 -0700

    Complete 0 test for countToTarget

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // Business Logic
 
 function countToTarget() {
+  event.preventDefault();
+  const inputString = parseInt(document.getElementById("numInput").value);
+  let stepsToTarget = [];
+  if (inputString === 0) {
+    stepsToTarget.push(0);
+    return stepsToTarget;
 
+  };
+  return stepsToTarget;
 };
 
 function runRoboRules() {
