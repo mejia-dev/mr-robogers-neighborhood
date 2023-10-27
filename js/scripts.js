d3638a850fc0278ff09f8c398a133c67b9c7cb0c
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-27 10:19:01 -0700

    Complete runRoboRules() test for number 1. Refactor code to remove unnecessary checks in the if statements

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,10 @@ function countToTarget() {
 function runRoboRules(number) {
   if (number.toString().includes("3")) {
     return "Won't you be my neighbor?"
-  } else if (number.toString().includes("2") && !number.toString().includes("3")) {
+  } else if (number.toString().includes("2")) {
     return "Boop!"
+  } else if (number.toString().includes("1")) {
+    return "Beep!"
   }
 };
 
