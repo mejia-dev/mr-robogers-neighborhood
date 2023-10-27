b0f7553478c3718de19ab61dfea97dff235b072e
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-27 09:40:35 -0700

    Add layout to scripts.js. Define event listener for when submit button on form is pressed

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Business Logic
+
+function countToTarget() {
+
+};
+
+function runRoboRules() {
+
+};
+
+
+// UI Logic
+
+function displayOutput() {
+
+};
+
+window.addEventListener("load", function () {
+  this.document.getElementById("inputForm").addEventListener("submit", countToTarget)
+});
